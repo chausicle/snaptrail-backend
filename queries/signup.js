f3d75933e46d668be8467c9de603c15af4afd607
Robson Farias <fariasrobson.r@gmail.com>
2018-05-01 18:03:34 -0700

    refract signup with async

--- queries/signup.js
+++ queries/signup.js
@@ -1,33 +1,32 @@
 const knex = require('./db');
 const bcrypt = require('bcryptjs');
 
+const createAccount = async (user_image, username, email, password) => {
+  console.log('username is ', username);
 
-const createAccount = (username, email, password, user_image) => {
-  return knex("users")
-  .select("*")
-  .where({ username })
-  .first()
-  .then(found => {
-    if (found) {
-      return 'Account already exist'
-    } else {
-      const hashedPassword = hash(password, 10)
+  const user = await knex("users")
+    .select("*")
+    .where({ username })
+    .orWhere({ email })
+    .first()
 
-      return knex('users')
-        .insert ({username, email, password: hashedPassword, user_image})
-        .into('users')
-        .returning('*')
-    }
-  })
-}
-  const hash = (password, saltRounds) => {
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(`${password}`, salt);
-    return `${hash}`
+  if (user) {
+    console.log("User ====>", user );
+    return false
+  } else {
+    const hashedPassword = hash(password, 10)
+    console.log('hashed', hashedPassword);
+    return knex('users')
+      .insert({username, email, password: hashedPassword, user_image})
+      .into('users')
+      .returning('*')
   }
+}
 
+const hash = (password, saltRounds) => {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(`${password}`, salt);
+  return `${hash}`
+}
 
-
-
-
-module.export = { createAccount }
+module.exports = { createAccount }
