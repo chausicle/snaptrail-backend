44b43ca4c78902b264b18029c8664a150fa89f09
Robson Farias <fariasrobson.r@gmail.com>
2018-05-08 12:07:59 -0700

    Fixed comments seeds by adding comment

--- seeds/04-comments.js
+++ seeds/04-comments.js
@@ -7,17 +7,20 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           user_id: 1,
-          post_id: 5
+          post_id: 5,
+          comment: 'I always wanted to go to Cowles Mountain Trail, I been looking for a moderate hiking trail near San Diego. Thanks for share Chausicle!'
         },
         {
           id: 2,
           user_id: 2,
-          post_id: 1
+          post_id: 1,
+          comment: 'I just moved to San Francisco and this Bernal Heights Park Trail works perfect for me, the location is near my house. Thanks Hobbyrob!'
         },
         {
           id: 3,
           user_id: 3,
-          post_id: 3
+          post_id: 3,
+          comment: 'Wow Maria!!! I have to check it out Glenwood Canyon. Your photo looks amazing!'
         }
       ]);
     })
