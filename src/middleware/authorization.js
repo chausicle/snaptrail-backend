817710b0e497cf8833546954f29961cf0c38ccd0
Justin Chau <chausicle@gmail.com>
2018-05-08 14:52:40 -0700

    Add additional middleware for checking authorization

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,17 +1,38 @@
-const verifyToken = (req, res, next) => {
-  const jwt = require("jsonwebtoken")
-  const secret = process.env.AWESOME_SECRET
-  const token = req.headers.authorization
-  const verified = jwt.verify(token, secret, (err, result) => {
-    if (err !== null) {
-      return next({
-        error: {
-          status: 403,
-          message: "JWT did not verify"
-        }
-      })
-    }
-  })
+const jwt = require("jsonwebtoken")
 
-  next()
+const checkForToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    res.sendStatus(403);
+  } else {
+    next();
+  }
+}
+
+const parseToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    req.token = token;
+    next();
+  } catch(err) {
+    res.sendStatus(401);
+  }
+}
+
+const verifyToken = (req, res, next) => {
+  try {
+    const decoded = jwt.verify(req.token, TOKEN_SECRET);
+    if (!decoded.loggedIn) {
+      res.sendStatus(403);
+      return;
+    }
+    next();
+  } catch(err) {
+    res.sendStatus(403);
+  }
+}
+
+module.exports = {
+  checkForToken,
+  parseToken,
+  verifyToken
 }
