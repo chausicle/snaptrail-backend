61cd7c06b6b6199e51be643727fa5d8862ab0422
Justin Chau <chausicle@gmail.com>
2018-05-08 22:26:40 -0700

    Add createComment for Comment route

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,26 +1,35 @@
-const model = require("../models/comments")
-const decode = require("jwt-decode")
+const model = require("../models/comments");
+const decode = require("jwt-decode");
 
 const getAllComments = async (req, res, next) => {
-  const comments = await model.getAllComments()
+  const comments = await model.getAllComments();
 
   if (comments.error) {
     return next({
       error: comments.error,
       status: comments.status,
       message: comments.message
-    })
+    });
   }
 
-  res.status(200).json(comments)
-}
+  res.status(200).json(comments);
+};
 
 const createComment = async (req, res, next) => {
-  const id = decode(req.token).sub.id
-  const comment = await model.createComment(req.body, id)
-}
+  const newComment = await model.createComment(req.body);
+
+  if (newComment.error) {
+    return next({
+      error: newComment.error,
+      status: newComment.status,
+      message: newComment.message
+    });
+  }
+
+  res.status(200).json(newComment);
+};
 
 module.exports = {
   getAllComments,
   createComment
-}
+};
