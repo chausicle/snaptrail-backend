be26753312a0aca47a8a8ffc9e053e601ce5fbbe
Justin Chau <chausicle@gmail.com>
2018-05-11 19:29:25 -0700

    Join Comments with Users

--- queries/comments.js
+++ queries/comments.js
@@ -1,8 +1,26 @@
 const knex = require("./db");
 
 const getAllComments = () => {
+  let result;
+
   return knex("comments")
-    .select("*");
+    .select("*")
+    .then(comments => {
+      result = comments
+      return knex("users")
+        .select(
+          "users.id",
+          "users.email",
+          "users.username",
+          "users.user_image"
+        )
+        .then(users => {
+          comments.forEach((comment, index) => {
+            result[index].user = users.find(user => user.id === comment.user_id)
+          });
+          return result;
+        });
+    });
 };
 
 const createComment = (body) => {
